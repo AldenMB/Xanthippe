4519eeaf6e66c489fcc8bf82707b31172508ff13
Alden <aldenbradford@gmail.com>
2022-12-24 15:00:10 -1000

    driver for lcd display

---
+++ driver/lcd.py
@@ -0,0 +1,77 @@
+import gpiozero
+import time
+
+default_pins = {
+    "led": 13,
+    "e": 14,
+    "rs": 15,
+    "d4": 16,
+    "d5": 17,
+    "d6": 18,
+    "d7": 19,
+}
+
+
+class LCD:
+    def __init__(self, pins=default_pins):
+        self.led = gpiozero.PWMLED(pins["led"])
+        self.data = [
+            gpiozero.DigitalOutputDevice(pins[p]) for p in "d4 d5 d6 d7".split()
+        ]
+        self.clock = gpiozero.DigitalOutputDevice(pins["e"])
+        self.select = gpiozero.DigitalOutputDevice(pins["rs"])
+
+        # configuration
+        self.select.off()
+        for byte in [
+            0x33,  # 8 bit interface
+            0x32,  # 8 bit interface
+            0x28,  # 4 bit interface, 2 display lines, 8px font
+            0x0C,  # display on, cursor off, blinking off
+            0x06,  # left-to-right mode
+        ]:
+            self.send_byte(byte)
+        self.clear()
+        self.led.value = 0.1
+
+    def clear(self):
+        self.select.off()
+        self.send_byte(0x01)
+        return self
+
+    def goto_line(self, line):
+        self.select.off()
+        self.send_byte(0x80 if line == 0 else 0xC0)
+        return self
+
+    def clockpulse(self):
+        time.sleep(0.0001)
+        self.clock.on()
+        time.sleep(0.0005)
+        self.clock.off()
+        time.sleep(0.0005)
+        return self
+
+    def write(self, message, line=None):
+        if line is not None:
+            self.goto_line(line)
+        self.select.on()
+        for char in message:
+            self.send_byte(ord(char))
+        return self
+
+    def send_byte(self, byte):
+        lo = byte & 0xF
+        hi = byte >> 4
+
+        for nibble in hi, lo:
+            for i, pin in enumerate(self.data):
+                pin.value = 1 & (nibble >> i)
+            self.clockpulse()
+
+
+if __name__ == "__main__":
+    lcd = LCD()
+    lcd.write("This is a test")
+    lcd.write("of the LCD scrn.", line=1)
+    time.sleep(1)
