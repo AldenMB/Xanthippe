846cd0b40cd1d8f3deaee740581f59c8ef9bd31b
Alden <aldenbradford@gmail.com>
2023-01-20 11:56:02 -0500

    log only changes

--- driver/buttonpress.py
+++ driver/buttonpress.py
@@ -1,22 +1,26 @@
 import binascii
 import gpiozero
+import time
 
 
 class ButtonPresser:
     def __init__(self):
         self.power = gpiozero.OutputDevice(20, initial_value=True)
-        self.enable = gpiozero.DigitalOutputDevice(21, active_high = False, initial_value = False)
-        self.data = [
-            gpiozero.OutputDevice(i) for i in range(22, 28)
-        ]
-        
-    def send(self, symbol, press_time = 0.05, between_time = 0.0):
+        self.enable = gpiozero.DigitalOutputDevice(
+            21, active_high=False, initial_value=False
+        )
+        self.data = [gpiozero.OutputDevice(i) for i in range(22, 28)]
+
+    def send(self, symbol):
         binary = binascii.a2b_base64("AAA" + symbol)[-1]
-        values = (int(d) for d in f'{binary:06b}'[::-1])
+        values = (int(d) for d in f"{binary:06b}"[::-1])
         for pin, value in zip(self.data, values):
             pin.value = value
-        self.enable.blink(on_time = press_time, off_time = between_time, n=1, background=True)
-        
+        self.enable.blink(on_time=0.05, off_time=0.1, n=1, background=False)
+        if symbol == "3":
+            # reset takes additional time for some reason
+            time.sleep(0.35)
+
 
 if __name__ == "__main__":
     bp = ButtonPresser()
--- driver/read_lcd.py
+++ driver/read_lcd.py
@@ -3,6 +3,9 @@ import time
 from dataclasses import dataclass
 
 
+LCD_REFRESH_PERIOD = 24_000_000  # milliseconds -> nanoseconds
+
+
 def normalize_reading(bits):
     """
     Pack the raw bits into one byte per segment. Discard the checksum.
@@ -14,70 +17,48 @@ def normalize_reading(bits):
                            0b00000001100000000000000000001110]).hex(' ')
     '60 00 00 63 7a 00 00 00 00 00 00 00 00 00'
     """
-    rows = [f'{x>>4:028b}' for x in bits]
+    rows = [f"{x>>4:028b}" for x in bits]
     columns = zip(*rows)
-    paired = zip(*[iter(columns)]*2)
-    return bytes(int(''.join(a+b), base=2) for a, b in paired)
-    
+    paired = zip(*[iter(columns)] * 2)
+    return bytes(int("".join(a + b), base=2) for a, b in paired)
+
+
 @dataclass
 class LogEntry:
     """one thing that has been shown on screen"""
+
     shown: int
     start: int
     end: int = None
-    
-    def close(self, time):
+
+    def __post_init__(self):
         if self.end is None:
-            self.end = time
-
-
-class LCDLog:
-    def __init__(self):
-        self.history = []
-        self.showing = None
-        self.most_recent_beat = None
-        self.pulserate = 24_000_000 # 24 milliseconds -> nanoseconds
-    
-    
-    def append(self, reading):
-        (time, *_), bits = zip(*reading)
-        showing = normalize_reading(bits)
-        if self.showing == showing and time - self.most_recent_beat < 1.5*self.pulserate:
-            self.most_recent_beat = time
-            return
-        
-        try:
-            self.history[-1].close(time)
-        except IndexError:
-            pass
-            
-        self.history.append(LogEntry(showing, time))
-        self.showing = showing
-        self.most_recent_beat = time
-
+            self.end = self.start
 
 
 class LCDReader:
     def __init__(self):
-        self.sample = gpiozero.OutputDevice(0, active_high = False, initial_value = False)
+        self.sample = gpiozero.OutputDevice(0, active_high=False, initial_value=False)
         self.clock = gpiozero.OutputDevice(1)
-        self.data = gpiozero.InputDevice(2, pull_up = True)
+        self.data = gpiozero.InputDevice(2, pull_up=True)
         self.triggers = [
-            gpiozero.DigitalInputDevice(i, pull_up = True) for i in range(6, 2, -1)
+            gpiozero.DigitalInputDevice(i, pull_up=True) for i in range(6, 2, -1)
         ]
         for i, trig in enumerate(self.triggers):
+
             def interrupt(*args, i=i):
                 self.on_interrupt(i)
+
             trig.when_deactivated = interrupt
-        
+
         self.partial_readings = []
-        self.reading_log = []
-        
+        self.log = []
+
     def on_interrupt(self, i):
         now = time.monotonic_ns()
         if i == 0:
             self.partial_readings = []
-        else:            
+        else:
             if len(self.partial_readings) != i:
                 self.partial_readings = []
                 return
@@ -85,18 +66,29 @@ class LCDReader:
             if now - then > 6_000_000:
                 self.partial_readings = []
                 return
-            
+
         partial = self.acquire()
         checksum = 15 & ~partial
         if checksum != 8 >> i:
             return
-        
+
         self.partial_readings.append((now, partial))
         if i == 3:
-            self.reading_log.append(self.partial_readings)
-            self.partial_readings = []        
-            
-        
+            self.record()
+            self.partial_readings = []
+
+    def record(self):
+        (time, *_), bits = zip(*self.partial_readings)
+        showing = normalize_reading(bits)
+        if (
+            self.log
+            and self.log[-1].shown == showing
+            and time - self.log[-1].end < 1.5 * LCD_REFRESH_PERIOD
+        ):
+            self.log[-1].end = time
+        else:
+            self.log.append(LogEntry(showing, time))
+
     def acquire(self):
         result = 0
         self.sample.off()
@@ -106,33 +98,25 @@ class LCDReader:
             self.clock.on()
         self.sample.on()
         return result
-            
-        
-        
+
+
 if __name__ == "__main__":
     from buttonpress import ButtonPresser
+
     bp = ButtonPresser()
-    
-    bp.send('3') # reset
-    time.sleep(0.5)
-    bp.send('4') # on
-    time.sleep(0.15)
-    bp.send('b') # 3
-    time.sleep(0.15)
-    bp.send('j') # 4
-    time.sleep(0.15)
-    
+
+    bp.send("3")  # reset
+    bp.send("4")  # on
+    bp.send("b")  # 3
+    bp.send("j")  # 4
+
     reader = LCDReader()
     time.sleep(0.5)
-    
-    print(f'{len(reader.reading_log)=}')
-    distinct = set(tuple(list(zip(*entry))[1]) for entry in reader.reading_log)
-    result = '\n'.join(f'{x:032b}' for dd in distinct for x in dd)
-    print(result)
-    assert result == '''00000000010000000000000000000111
-10000010100000000000000000001011
-10000011110000000000000000001101
-00000001100000000000000000001110'''
-    
-    
-    
+
+    print(f"{len(reader.log)=}")
+    print(f"{reader.log=}")
+
+    for entry in reader.log:
+        print(entry.shown.hex(" "))
+        print(f"start={entry.start}")
+        print(f"duration={entry.end-entry.start}")
