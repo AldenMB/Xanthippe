746438a2a9089cde5b6ca56e903cdb043dfd4054
Alden <aldenbradford@gmail.com>
2023-01-26 15:30:41 -0500

    sort the records by length

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 rm backup.db
 sqlite3 xanthippe.db "VACUUM INTO 'backup.db';"
-sqlite3 backup.db --csv "SELECT buttons, QUOTE(screen) AS screen, requested FROM sessions ORDER BY buttons;" | gzip > backup.csv.gz
+sqlite3 backup.db --csv "SELECT buttons, QUOTE(screen) AS screen, requested FROM sessions ORDER BY LENGTH(buttons), buttons;" | gzip > backup.csv.gz
